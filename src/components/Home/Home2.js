1649c6b5f76513d1f1d3848b93a9dc18327f9e7a
Dylan66 <dylannganga@gmail.com>
2025-08-06 13:23:35 +0300

    changes to intro section

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -18,24 +18,21 @@ function Home2() {
             <h1 style={{ fontSize: "2.6em" }}>
               LET ME <span className="purple"> INTRODUCE </span> MYSELF
             </h1>
-            <p className="home-about-body">
-              I am passionate about technology and all the potential it has to change society.
-              <br />
-              <br />
-              I am competent in high-level programming languages like
-              <b className="purple"> Python, JavaScript, and Go. </b>
-              <br />
-              <br />
-              My fields of interest are building frontend and backend
-              <b className="purple"> Web Technologies and Products. </b>
-              I also have a keen interest in
-              <b className="purple"> Blockchain smart contract development.</b>
-              <br />
-              <br />
-              I apply my passion for developing products with Python web frameworks such as <b className="purple">FastAPI and Django</b> and
-              JavaScript Libraries and Frameworks
-              &nbsp;like
-              <b className="purple"> React, Vue, and Next.js</b>
+            <p 
+              className="home-about-body" 
+              style={{ 
+                fontFamily: 'Segoe UI, Arial, sans-serif', 
+                fontSize: '1.18em', 
+                lineHeight: 1.85, 
+                wordSpacing: '0.12em',
+                letterSpacing: '0.02em',
+                marginBottom: 0
+              }}
+            >
+              I am passionate about technology and all the potential it has to change society.<br /><br />
+              I am competent in high-level programming languages like <b className="purple">Python, JavaScript, and Go</b>.<br /><br />
+              My fields of interest are building frontend and backend <b className="purple">Web Technologies and Products</b>. I also have a keen interest in <b className="purple">Blockchain smart contract development</b>.<br /><br />
+              I apply my passion for developing products with Python web frameworks such as <b className="purple">FastAPI and Django</b> and JavaScript libraries and frameworks like <b className="purple">React, Vue, and Next.js</b>.
             </p>
           </Col>
           <Col md={4} className="myAvtar">
